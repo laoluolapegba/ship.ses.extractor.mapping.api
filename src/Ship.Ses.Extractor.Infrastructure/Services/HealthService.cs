bf631abcd1a675fbb164b7914157e29bdd61e7e0
Olaoluwa Olapegba <laoluolapegba@gmail.com>
2025-08-05 20:19:27 +0100

    chore : change healthcheck db connection

--- src/Ship.Ses.Extractor.Infrastructure/Services/HealthService.cs
+++ src/Ship.Ses.Extractor.Infrastructure/Services/HealthService.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging;
 using Ship.Ses.Extractor.Application.Interfaces;
 using Ship.Ses.Extractor.Domain.Entities.DataMapping;
+using Ship.Ses.Extractor.Infrastructure.Persistance.Repositories;
 using System.Data.Common;
 using System.Threading.Tasks;
 
@@ -13,22 +14,24 @@ namespace Ship.Ses.Extractor.Infrastructure.Services
     public class HealthService : IHealthService
     {
         private readonly ILogger<HealthService> _logger;
+        private readonly EmrPersistenceFactory _dbContextFactory;
         private readonly string _connectionString;
         private readonly string _dbType;
 
-        public HealthService(IConfiguration configuration, ILogger<HealthService> logger)
+        public HealthService(IConfiguration configuration, ILogger<HealthService> logger, EmrPersistenceFactory dbContextFactory)
         {
             _logger = logger;
-            _connectionString = configuration["EmrDatabase:ConnectionString"];
-            _dbType = configuration["EmrDatabase:Type"] ?? "Unknown";
+            //_connectionString = configuration["EmrDatabase:ConnectionString"];
+            //_dbType = configuration["EmrDatabase:Type"] ?? "Unknown";
+            _dbContextFactory = dbContextFactory;
         }
 
         public async Task<HealthResult> CheckHealthAsync()
         {
             try
             {
-                using var connection = CreateDbConnection();
-
+                //using var connection = CreateDbConnection();
+                using var connection = _dbContextFactory.MakeConn();
                 await connection.OpenAsync();
                 using var command = connection.CreateCommand();
                 command.CommandText = "SELECT 1";
@@ -38,8 +41,8 @@ namespace Ship.Ses.Extractor.Infrastructure.Services
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "❌ Health check failed for relational DB ({DbType})", _dbType);
-                return HealthResult.Unhealthy($"Relational DB connection failed: {ex.Message}");
+                _logger.LogError(ex, "❌ Health check failed for DB");
+                return HealthResult.Unhealthy($"DB connection failed: {ex.Message}");
             }
         }
 
